649a9cda8104d947d9079250eae31845c604f962
Vitalik-D <vitalikdyaduchko@gmail.com>
2019-04-09 09:55:23 +0300

    push heroku

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const index = require('http').createServer(app);
+const io = require('socket.io')(index);
+const port = process.env.PORT || 3000;
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(3000, function(){
+index.listen(port, function(){
     console.log('listening3000');
 });
 
